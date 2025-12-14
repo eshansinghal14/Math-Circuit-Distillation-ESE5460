6900f91d7ad45c33083fffc4fa25de054901ea33
eshansinghal14 <eshansinghal05@gmail.com>
2025-12-14 18:25:09 -0500

    wrote activations generation code

--- src/benchmark.py
+++ src/benchmark.py
@@ -2,11 +2,10 @@ from utils import load_model, test_model, eval_model, gen_2d_add_dataset
 import json
 import sys
 
-if __name__ == '__main__':
-    gen_2d_add_dataset('../datasets/2d_add_train.json', samples=5000)
-    gen_2d_add_dataset('../datasets/2d_add_test.json', samples=None)
+# gen_2d_add_dataset('../datasets/2d_add_train.json', samples=5000)
+# gen_2d_add_dataset('../datasets/2d_add_all.json', samples=None)
 
-    # model, tokenizer = load_model(sys.argv)
-    # test_model(model, tokenizer, 'datasets/2d_add_test.json', 'results/2d_add_test.json')
+# model, tokenizer = load_model(sys.argv)
+# test_model(model, tokenizer, 'datasets/2d_add_all.json', f'../results/{sys.argv[1]}/2d_add_all.json')
 
-    # eval_model('../results/2d_add_test.json')
+# eval_model('../datasets/2d_add_test.json')
