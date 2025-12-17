1076127baa024addc1f98e9f6c7ec6f3a1a795ec
Ubuntu <ubuntu@ip-172-31-92-237.ec2.internal>
2025-12-17 05:36:58 +0000

    impl load_custom_model

--- src/neuron_clustering.py
+++ src/neuron_clustering.py
@@ -1,6 +1,17 @@
+import io
+import torch
 import boto3
+
 from constants import BUCKET_NAME
+from circuit_discovery.models import CircuitDiscoveryModel
+from utils import load_custom_model
 
 s3 = boto3.client("s3")
-model_name = 'model_2000'
-obj = s3.get_object(Bucket=BUCKET_NAME, Key=key)
+
+model_name = "model_2000"
+model = load_custom_model(model_name)
+model.eval()
+
+def corr_distance_sanity_check():
+    pass
+
