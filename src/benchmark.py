af30805b10e23831c8a1bbb755bef6b1075a0a53
Ubuntu <ubuntu@ip-172-31-87-84.ec2.internal>
2025-12-18 19:23:01 +0000

    clustered 1b, 8b, ablated 1b

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,15 +1,18 @@
-from utils import load_model, get_model_name, test_model, eval_model, gen_2d_add_dataset
+from utils import load_model, get_model_name, test_model, eval_model, gen_2d_add_dataset, gen_3d_add_dataset
 import json
 import sys
 
-# model, tokenizer = load_model(sys.argv)
-# model_name = get_model_name(sys.argv)
+model_name = get_model_name(sys.argv)
+model, tokenizer = load_model(model_name)
 
 # gen_2d_add_dataset('../datasets/2d_add_train.json', samples=5000)
 # gen_2d_add_dataset('../datasets/2d_add_all.json', samples=None, tokenizer=tokenizer)
 # gen_2d_add_dataset('../datasets/2d_add_test_20.json', samples=None, tokenizer=tokenizer)
+# gen_3d_add_dataset('../datasets/3d_add_test.json', samples=1000, tokenizer=tokenizer)
 
-# test_model(model, tokenizer, '../datasets/2d_add_test_20.json', f'../results/{model_name}/2d_add_test_20.json')
+dataset = '3d_add_test.json'
+# test_model(model, tokenizer, f'../datasets/{dataset}', f'../results/{model_name}/{dataset}')
 
-eval_model('../results/meta-llama/Meta-Llama-3-8B/2d_add_all.json')
-eval_model('../results/meta-llama/Llama-3.2-1B/2d_add_all.json')
+eval_model(f'../results/meta-llama/Meta-Llama-3-8B/{dataset}')
+eval_model(f'../results/meta-llama/Llama-3.2-1B/{dataset}')
+eval_model(f'../results/vedantgaur/circuit-distilled-llama-1b-low-cka/{dataset}')
