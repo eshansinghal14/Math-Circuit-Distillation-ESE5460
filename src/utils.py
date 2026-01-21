784682fabdc633973221292355c25747b901dcda
eshansinghal14 <eshansinghal05@gmail.com>
2026-01-21 14:53:11 -0500

    fixed import

--- src/utils.py
+++ src/utils.py
@@ -21,7 +21,7 @@ def get_model_name(argv):
 def load_model(model_name):
     hf_logging.set_verbosity_error()
 
-    from transformers import AutoModelForCausal, AutoTokenizer
+    from transformers import AutoModelForCausalLM, AutoTokenizer
     from huggingface_hub import login
 
     global logged_in
