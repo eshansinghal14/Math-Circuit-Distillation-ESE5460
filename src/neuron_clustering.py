4e249f29a1a3e73d186b9e37f0bbaae2e20cbac0
eshansinghal14 <eshansinghal05@gmail.com>
2026-02-26 15:17:17 -0500

    update clusters range

--- src/neuron_clustering.py
+++ src/neuron_clustering.py
@@ -297,7 +297,7 @@ if __name__ == "__main__":
         if neuron_masks[subclass].any().item():
             print(f"Processing subclass {subclass}")
             k_gs_testing[subclass] = {}
-            for k in range(1, 14):
+            for k in range(1, 20, 2):
                 _, _, loss = run_neuron_kmeans(k, subclass=subclass, log=False)
                 k_gs_testing[subclass][k] = loss
                 print(f"Subclass {subclass}, k={k}, loss={loss}")
