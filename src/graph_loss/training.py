1f5385869eeb61f0bab9012249545d8f793bbf6f
eshansinghal14 <eshansinghal05@gmail.com>
2026-05-19 00:56:01 -0500

    fix

--- src/graph_loss/training.py
+++ src/graph_loss/training.py
@@ -142,7 +142,7 @@ def compute_prompt_graph_loss(
             mlp_input_cache=student_mlp_input_cache,
             anova_range_radius=config.student_anova_range_radius,
             anova_nodes_per_label=config.student_anova_nodes_per_label,
-            min_specificity=config.student_sum_min_specificity,
+            sum_min_specificity=config.student_sum_min_specificity,
             activation_write_result_cache=config.activation_write_result_cache,
         )
     student_supergraph = _aggregate_supergraph_adjacency(
